0c751a962036590e693dc66c24a998fc891feac2
rogerio monteiro sousa <rogeriomonteiros@gmail.com>
2022-02-25 17:54:50 -0300

    Ajuste no titulo da suite home para padrao

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,5 @@
 describe('home page', () => {
-    it('app deve estar on line', () => {
+    it('App deve estar on line', () => {
         cy.visit('/')
         cy.get('#page-home main h1').should('have.text','Seja um parceiro entregador pela Buger Eats')
     })
